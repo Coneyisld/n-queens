e925aeb570935ddb9f0b234d5c9f1de3fc593fcd
Hack Reactor Student <student@hackreactor.com>
2017-08-30 15:59:15 -0700

    passing row conflict tests

--- src/Board.js
+++ src/Board.js
@@ -79,11 +79,26 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var row = this.get(rowIndex);
+      var count = 0;
+
+      for (var i = 0; i < row.length; i++) {
+        count += row[i];
+      }
+
+      return count > 1;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
+      var rows = this.rows();
+
+      for (var i = 0; i < rows.length; i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;  
+        }
+      }      
+
       return false; // fixme
     },
 
